04b9a8ee1ee35b6f67093a1304756a5da4f99a18
we2423hd <ckiefer15@winona.edu>
2018-01-14 00:25:49 -0600

    Added file for solving rabbits and recurrence relations problem

--- TranslatingRNAIntoProtein.py
+++ TranslatingRNAIntoProtein.py
@@ -1,7 +1,7 @@
-f = open("/Users/we2423hd/Documents/untitled5/rosalind_prot.txt")
+f = open("/Users/we2423hd/Documents/Rosalind/rosalind_prot.txt")
 
 translationTable = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
-    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
     "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
     "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
     "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
@@ -17,13 +17,13 @@ translationTable = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
     "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
     "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
 
-results = open("/Users/we2423hd/Documents/Bioinformatics-Practice/Results.txt", mode='w')
+results = open("/Users/we2423hd/Documents/Rosalind/Results.txt", mode='w')
 while True:
     codon = f.read(3).strip()
     if not codon:
         break
-    elif (codon == '\n'):
-        break
+    #elif (codon == '\n'):
+     #   break
     elif (codon == 'UAG' or codon == 'UAA' or codon == 'UGA'):
         results.write("")
     else:
